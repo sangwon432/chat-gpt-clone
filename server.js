6db32ff5c734e96805a37470a3140972392a8f39
Sangwon Lee <sangwonlee@Sangwons-MacBook-Air.local>
2023-08-24 20:01:44 -0500

    Add initial project files and setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+//dotenv
+dotenv.config()
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 8080
+
+//listen server
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.DEV_MODE} on ${PORT}`.bgCyan.white);
+});
+
